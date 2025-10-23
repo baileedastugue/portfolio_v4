528b876e3b43a2890a5b3bcc4f13b7dfc71c0531
Bailee Dastugue <bailee.dast@gmail.com>
2025-10-23 09:25:59 -0500

    open in new for tagline links and icon colors for nav when they aren't sticky to the top

--- client/src/data/experience.ts
+++ client/src/data/experience.ts
@@ -5,7 +5,7 @@ export const experienceData: Company[] = [
     name: 'H-E-B',
     roles: [
       {
-        title: 'Technical product Manager',
+        title: 'Technical Product Manager',
         startDate: 'February 2025',
         current: true,
         tagline:
